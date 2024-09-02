ccf72b4ae9c677038ec9d7a565e53be470aa762d
MetinBiter <145712932+Teyon-Tet@users.noreply.github.com>
2024-09-02 21:47:44 +0300

    api responses and data transfer layer

--- BankingPlatform/loans/src/main/java/com/eazybytes/loans/entity/Loans.java
+++ BankingPlatform/loans/src/main/java/com/eazybytes/loans/entity/Loans.java
@@ -1,9 +1,17 @@
 package com.eazybytes.loans.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
 
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Loans extends BaseEntity {
 
     @Id
